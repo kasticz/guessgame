59c3dc35d374d7b4b19d5bbd2351deee1ff04149
kasticz <den-sendmen@yandex.ru>
2022-10-13 17:41:28 +0300

    another readme fix,head fix

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home(props) {
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta charSet="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Что было раньше?</title>
       </Head>
